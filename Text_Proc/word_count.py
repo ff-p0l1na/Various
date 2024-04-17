1e410fd5d5851d87d4501a6df0c3181e6c2cf14a
Paulina Faltyn <paulinafaltyn@Luigi.local>
2024-04-17 19:55:20 +0200

    first commit

---
+++ Text_Proc/word_count.py
@@ -0,0 +1,37 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import string
+import pandas as pd
+import sys
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+# Read the input file from the command line argument
+input_file = sys.argv[1]
+
+# Tokenize the text and remove punctuation and stop words
+with open(input_file, 'r') as file:
+    data = file.read()
+    list_of_words = word_tokenize(data.lower())  # Convert to lowercase
+    stoplist = set(stopwords.words('english'))
+    stripped_list = [word.strip(string.punctuation) for word in list_of_words if word.strip(string.punctuation) not in stoplist]
+
+# Count occurrences of each word
+words_counted = {}
+for word in stripped_list:
+    if word in words_counted:
+        words_counted[word] += 1
+    else:
+        words_counted[word] = 1
+
+# Convert the dictionary to a DataFrame
+df = pd.DataFrame(words_counted.items(), columns=['WORD', 'COUNT'])
+
+# Sort the DataFrame by count in descending order
+df = df.sort_values(by='COUNT', ascending=False)
+
+# Write the DataFrame to a TSV file
+output_file = f"{input_file.split(sep='.')[0]}_word_count.tsv"
+df.to_csv(output_file, sep='\t', index=False)
