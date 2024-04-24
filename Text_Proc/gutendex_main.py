d11b851712f28da6f9fcb81ad04c973282e3c236
Paulina Faltyn <paulinafaltyn@Luigi.local>
2024-04-24 19:33:25 +0200

    Gutendex: downloading a single specific book using its ID works

--- Text_Proc/gutendex_main.py
+++ Text_Proc/gutendex_main.py
@@ -1,22 +1,37 @@
 import requests
+import wget
+data_results = []
 
 def FindBooks(language, year):
-
-    url = f"https://gutendex.com/books?author_year_start={year}&languages={language}"
+    global data_results
+    url = f"https://gutendex.com/books?author_year_start={year}&author_year_end={year}&languages={language}"
 
     # Make the API request
     response = requests.get(url)
-
-    # Check if request was successful
-    if response.status_code == 200:
-        # Parse the JSON response
+    if response.status_code == 200: # Check if request was successful
         data = response.json()
-        print(data['results']) # results is a list of dicts; each dict contains info about 1 book
-        data_res = data['results'] # data_res is now a LIST of dicts
-        for item in data_res:
-            print(f"ID: {item['id']} TITLE: {item['title']}")
+        # value of 'results' is a LIST of dicts; each dict contains info about 1 book;
+        data_results = data['results'] # data_results is now a LIST of dicts
+        print("Books found:\n")
+        for item in data_results:
+            print(f"ID: {item['id']}\tTitle: {item['title']}")
+
+        return data_results
     else:
         print("Failed to fetch books. Status code:", response.status_code)
 
-# Example usage
-FindBooks(language='en', year=2023)
+###
+
+language = input("Enter the language code, e.g. 'en': ")
+year = input("Enter the year: ")
+FindBooks(language, year)
+
+###
+
+def DownloadBook(id):
+    url = f'https://www.gutenberg.org/ebooks/{id}.txt.utf-8'
+    book_file = wget.download(url)
+    return book_file
+
+chosen_id = input("Enter chosen book's ID to download it: ")
+DownloadBook(chosen_id)
