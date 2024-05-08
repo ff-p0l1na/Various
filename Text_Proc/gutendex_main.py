dd2c793052aab9c7a7e380e1d44ea65fd7d960f4
Paulina Faltyn <paulinafaltyn@Luigi.local>
2024-05-08 19:05:54 +0200

    Try adding argparse instead of input prompts

--- Text_Proc/gutendex_main.py
+++ Text_Proc/gutendex_main.py
@@ -1,3 +1,4 @@
+import argparse
 import matplotlib.pyplot as plt
 import nltk
 from nltk.corpus import stopwords
@@ -14,9 +15,9 @@ nltk.download('stopwords')
 #####
 data_results = []
 #####
-def FindBooks(language, year):
+def FindBooks(language, year_start, year_end):
     global data_results
-    url = f"https://gutendex.com/books?author_year_start={year}&author_year_end={year}&languages={language}"
+    url = f"https://gutendex.com/books?author_year_start={year_start}&author_year_end={year_end}&languages={language}" # year start and end instead of one variable
 
     # Make the API request
     response = requests.get(url)
@@ -32,8 +33,8 @@ def FindBooks(language, year):
     else:
         print("Failed to fetch books. Status code:", response.status_code)
 
-def DownloadBook(id):
-    url = f'https://www.gutenberg.org/ebooks/{id}.txt.utf-8'
+def DownloadBook(id, file_type):
+    url = f'https://www.gutenberg.org/ebooks/{id}.{file_type}' # file type needs to be defined by user
     book_file = wget.download(url)
     return book_file
 
@@ -43,16 +44,34 @@ def PlotTheWordCloud():
     plt.axis('off')
     plt.show()
 
+"""
+remove this and use argparse flags instead
 # Prompt the user for language and year:
 language = input("Enter the language code, e.g. 'en': ")
 year = input("Enter the year: ")
+"""
+
+parser = argparse.ArgumentParser(description='Download books from Project Gutenberg')
+parser.add_argument('--language', type=str, required=True, help='Specify a desired language for looked books')
+parser.add_argument('--year_start', type=int, required=True, help='Specify a year for the start range of looked books')
+parser.add_argument('--year_end', type=int, required=True, help='Specify a year for the end range of looked books')
+args = parser.parse_args()
+
+language = args.language
+year_start = args.year_start
+year_end = args.year_end
+file_type = args.file_type
 
 # Find all the matching books:
-FindBooks(language, year)
+FindBooks(language, year_start, year_end)
 
 # Download the chosen book:
 chosen_id = input("Enter chosen book's ID to download it: ")
-book_file = DownloadBook(chosen_id)
+book_file = DownloadBook(chosen_id, file_type)
+
+"""
+How to handle choosing a file format? look up example.json
+"""
 
 # Tokenize the text and remove punctuation and stop words
 with open(book_file, 'r') as file:
