01b4f7c64d9030f033f2afe4d9694ac33abf94ff
Paulina Faltyn <paulinafaltyn@Luigi.local>
2024-05-18 22:19:26 +0200

    Finish two_sums ex

--- NeetCode/two_sums.py
+++ NeetCode/two_sums.py
@@ -6,8 +6,8 @@
 
 # You can return the answer in any order.
 
-array_of_integers = [1, 45, 5, 533, 13, 0, 45, 7, 87]
-int_target = 14
+array_of_integers = [1, 9, 20, 21, 8, 45, 5, 533, 13, 0, 45, 7, 87]
+int_target = 29
 
 def makeSum(array, int_target):
 
@@ -19,7 +19,7 @@ def makeSum(array, int_target):
         else:
             integer_count[i] += 1
 
-# create a dict where each key is index of each integer:
+# create a dict where each key is index of each integer from the list:
     integer_index = {}
     for j in range(len(array)):
         integer_index[j] = array[j]
@@ -27,19 +27,18 @@ def makeSum(array, int_target):
     for num_1 in integer_count:
         num_2 = int_target - num_1
         if num_2 in integer_count:
-
-
+            for k in integer_index:
+                if num_1 == integer_index[k]:
+                    ix_1 = k
+                if num_2 == integer_index[k]:
+                    ix_2 = k
             print(f"""The numbers that make {int_target} and their indices are: 
-                {num_1} : xx \n
-                {num_2} : xx""")
+                {num_1} : {ix_1} \n
+                {num_2} : {ix_2}""")
             
             return num_1, num_2
+        
         else:
             continue
 
 makeSum(array_of_integers, int_target)
-
-        
-            
-
-
