1cf5abe404ac5c4c8ff369c0451b4417a4f03c58
Paulina Faltyn <paulinafaltyn@Luigi.local>
2024-05-22 19:26:12 +0200

    change default option, show only text files

--- Text_Proc/gutendex_main.py
+++ Text_Proc/gutendex_main.py
@@ -25,19 +25,32 @@ def FindBooks(language, year_start, year_end):
         data = response.json()
         # value of 'results' is a LIST of dicts; each dict contains info about 1 book;
         data_results = data['results'] # data_results is now a LIST of dicts
-        print("Books found:\n")
-        for item in data_results:
-            print(f"ID: {item['id']}\tTitle: {item['title']}")
+        # check if list is empty (no results). It happens in the default param, but can happen due to the user input too.
+        if not data_results:
+            print("No books found. Try different parameters.")
+            quit()
+        else:
+            print("Books found:\n")
+            for item in data_results:
+                if item['media_type'] == 'Text': # eg. 19681 is "Sound"
+                    print(f"ID: {item['id']}\tTitle: {item['title']}")
 
         return data_results
     
     else:
         print("Failed to fetch books. Status code:", response.status_code)
 
-def DownloadBook(id, file_type):
+def DownloadBook(id):
 
-    # url = ??? how to handle url? look at example_urls.txt
-    book_file = wget.download(url)
+    url = f'https://www.gutenberg.org/ebooks/{id}.txt.utf-8'
+    try:
+        response = requests.get(url)
+        book_file = wget.download(url)
+    except:
+        print("Failed to download book. Status code:", response.status_code)
+        print(data_results)
+        quit()
+
     return book_file
 
 def PlotTheWordCloud():
@@ -46,8 +59,6 @@ def PlotTheWordCloud():
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
     plt.show()
-
-
 
 parser = argparse.ArgumentParser(description='Download books from Project Gutenberg')
 parser.add_argument('--language', type=str, required=False, help='Specify a desired language for looked books')
@@ -61,7 +72,8 @@ if language is None: # default option when there's no flag
 
 year_start = args.year_start
 if year_start is None: # default option when there's no flag
-    year_start = datetime.date.today().year
+    year_start = datetime.date.today().year - 1
+    # added -1 to get the previous year, cause the current year is too recent for any books
 
 year_end = args.year_end
 if year_end is None: # default option when there's no flag
@@ -72,7 +84,7 @@ FindBooks(language, year_start, year_end)
 
 # Download the chosen book:
 chosen_id = input("Enter chosen book's ID to download it: ")
-book_file = DownloadBook(chosen_id, file_type)
+book_file = DownloadBook(chosen_id)
 
 # Tokenize the text and remove punctuation and stop words
 with open(book_file, 'r') as file:
@@ -104,8 +116,6 @@ df.to_csv(output_file, sep='\t', index=False)
 
 # Create a word cloud
 wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(words_counted)
-
-#####
 
 PlotTheWordCloud()
 quit()
